57f605005a823edba7a5596883e04ee48a3e2363
lgomessantos02 <lgomessantos02@gmail.com>
2026-04-27 15:33:48 -0300

    Segundo commit

--- Ex31/Program.cs
+++ Ex31/Program.cs
@@ -1,4 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int a;
-int b;
+using System.ComponentModel.Design;
+
+int A;
+int B;
+
+Console.WriteLine("Digite o valor de A:");
+A = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Digite o valor de B:");
+B = int.Parse(Console.ReadLine());
+
+if (A > B)
+{
+   Console.WriteLine(" A é maior que B");
+}
+else
+{
+    Console.WriteLine("B é maior que A");
+}
